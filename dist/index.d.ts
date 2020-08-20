4e6ba28ada8768a2cb54ebd809a0adbfc73b0c5d
kooritea <ochatokori@gmail.com>
2020-08-20 20:27:46 +0800

    优化一大堆方法

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -9,20 +9,23 @@
 export declare const readInt: (buffer: ArrayBuffer, start: number, len: number, view?: 1 | 2 | 4) => number;
 /**
  * 写入无符号10进制正整数
+ * 会直接修改传入的arraybuffer
  * 请注意，如果写入长度大于一，将以大端在左的方式写入
  * @param buffer ArrayBuffer或bufferView，传入bufferView时view参数无效
  * @param start 从0开始,单位为view，比如view为4的时候，start为1表示从第5个字节(第33个二进制位)开始读取长度len*view个字节
  * @param len 该整数所占长度
  * @param value 10进制正整数 最大可写入的整数为2^(8*view*len)-1
  * @param view 将多少字节视为一个单位，默认为Uint8Array，分别对应Uint8Array|Uint16Array|Uint32Array
+ * @return ArrayBuffer 返回传入的arraybuffer引用
  */
-export declare const writeInt: (buffer: ArrayBuffer | Uint16Array | Uint8Array, start: number, len: number, value: number, view?: 1 | 2 | 4) => void;
+export declare const writeInt: (buffer: ArrayBuffer | Uint16Array | Uint8Array, start: number, len: number, value: number, view?: 1 | 2 | 4) => ArrayBuffer;
+export declare const writeHex: (target: ArrayBuffer, start: number, values: number[]) => ArrayBuffer;
 export declare const utf16beToUTF16le: (utf16be: Uint8Array) => Uint8Array;
 /**
  * 此方法会清除位于前面的字符顺序标记(只有utf16或传入Uint16Array才可能会有)
  * @param arrayBuffer ArrayBuffer或者view
  * @param encode 传入view时该项无效，ascii使用Uint8Array utf16使用Uint16Array
- * @param cleanZero 是否清除读取到的0
+ * @param cleanZero 是否清除非结尾的0
  */
 export declare const toString: (arrayBuffer: ArrayBuffer | Uint16Array | Uint8Array, encode?: 'ascii' | 'utf16le' | 'utf16be', cleanZero?: boolean) => string;
 /**
@@ -31,15 +34,18 @@ export declare const toString: (arrayBuffer: ArrayBuffer | Uint16Array | Uint8Ar
  * js默认使用小端在前(utf16le)
  * @param str
  * @param encode
+ * @param addBOM default false 添加fffe或feff的字节顺序控制符(只用utf16le/utf16be编码有效) 当str不为空字符串才有效
+ * @param addEnd default false 添加0或00的结束控制字符
  */
-export declare const toArrayBuffer: (str: string, encode: 'ascii' | 'utf16le' | 'utf16be') => ArrayBuffer;
+export declare const toArrayBuffer: (str: string, encode: 'ascii' | 'utf16le' | 'utf16be', addBOM?: boolean, addEnd?: boolean) => ArrayBuffer;
 /**
- * 从指定地方开始读取字符串，直到遇到0x00 或0x0000为止
- * 返回bufferView 不会清除字节顺序标记
- * ASCII时返回Uint8Array utf16时返回Uint16Array
+ * 从指定地方开始截取到0x00或0x0000为止
+ * 不会清除字节顺序标记
+ * 返回的arraybuffer不包括最后的0x00 或0x0000
  * @param arrayBuffer
  * @param start
  * @param encode
  */
-export declare const readStringToZero: (arrayBuffer: ArrayBuffer, start: number, encode: 'ascii' | 'utf16') => Uint16Array | Uint8Array;
+export declare const sliceToZero: (arrayBuffer: ArrayBuffer, start: number, encode: 'ascii' | 'utf16') => ArrayBuffer;
 export declare const bufferToArrayBuffer: (buffer: Buffer) => ArrayBuffer;
+export declare const concat: (...args: ArrayBuffer[]) => ArrayBuffer;
--- index.ts
+++ index.ts
@@ -30,14 +30,16 @@ export const readInt = function(buffer:ArrayBuffer,start:number,len:number,view:
 
 /**
  * 写入无符号10进制正整数  
+ * 会直接修改传入的arraybuffer  
  * 请注意，如果写入长度大于一，将以大端在左的方式写入
  * @param buffer ArrayBuffer或bufferView，传入bufferView时view参数无效
  * @param start 从0开始,单位为view，比如view为4的时候，start为1表示从第5个字节(第33个二进制位)开始读取长度len*view个字节
  * @param len 该整数所占长度
  * @param value 10进制正整数 最大可写入的整数为2^(8*view*len)-1
  * @param view 将多少字节视为一个单位，默认为Uint8Array，分别对应Uint8Array|Uint16Array|Uint32Array
+ * @return ArrayBuffer 返回传入的arraybuffer引用
  */
-export const writeInt = function(buffer:ArrayBuffer|Uint16Array|Uint8Array,start:number,len:number,value:number,view:1|2|4 = 1):void{
+export const writeInt = function(buffer:ArrayBuffer|Uint16Array|Uint8Array,start:number,len:number,value:number,view:1|2|4 = 1):ArrayBuffer{
   let bufferView:Uint8Array|Uint16Array|Uint32Array
   if(buffer instanceof ArrayBuffer){
     bufferView = getView(buffer,view)
@@ -45,7 +47,7 @@ export const writeInt = function(buffer:ArrayBuffer|Uint16Array|Uint8Array,start
     bufferView = buffer
   }
   if(value > (Math.pow(2,len*8*view) - 1)){
-    throw new Error(`Cannot write Int greater than 2^${len*8}-1`)
+    throw new Error(`Cannot write Int greater than 2^${len*8}-1: ${value}`)
   }
   if(bufferView.length < start + len){
     throw new Error(`Cannot write an Int of length ${len} at position ${start},because the input bufferView length is ${bufferView.length}`)
@@ -55,6 +57,15 @@ export const writeInt = function(buffer:ArrayBuffer|Uint16Array|Uint8Array,start
     bufferView[start + i] = value/Math.pow(Math.pow(2,8*view),len - i - 1)
     i++
   }
+  return buffer
+}
+
+export const writeHex = function(target:ArrayBuffer,start:number,values:number[]):ArrayBuffer{
+  const view = new Uint8Array(target)
+  for(let i=start;i<target.byteLength;i++){
+    view[i] = values[i-start]
+  }
+  return target
 }
 
 export const utf16beToUTF16le = function(utf16be:Uint8Array):Uint8Array{
@@ -70,7 +81,7 @@ export const utf16beToUTF16le = function(utf16be:Uint8Array):Uint8Array{
  * 此方法会清除位于前面的字符顺序标记(只有utf16或传入Uint16Array才可能会有)
  * @param arrayBuffer ArrayBuffer或者view
  * @param encode 传入view时该项无效，ascii使用Uint8Array utf16使用Uint16Array
- * @param cleanZero 是否清除读取到的0
+ * @param cleanZero 是否清除非结尾的0
  */
 export const toString = function(arrayBuffer:ArrayBuffer|Uint16Array|Uint8Array,encode:'ascii'|'utf16le'|'utf16be' = 'ascii',cleanZero:boolean = false):string{
   let dataString = "";
@@ -80,14 +91,14 @@ export const toString = function(arrayBuffer:ArrayBuffer|Uint16Array|Uint8Array,
       bufferView = <Uint8Array>getView(arrayBuffer,1)
     }else if(encode === 'utf16le'){
       bufferView = <Uint16Array>getView(arrayBuffer,2)
-      if(bufferView[0] === 0xFFFE){
+      if(readInt(arrayBuffer,0,2,1) === 65534){ // ff fe
         bufferView = bufferView.slice(1)
       }
       // utf16 要去除前两个判断高地位顺序的字节
     }else if(encode === 'utf16be'){
       bufferView = utf16beToUTF16le(<Uint8Array>getView(arrayBuffer,1))
       bufferView = <Uint16Array>getView(bufferView.buffer,2)
-      if(bufferView[0] === 0xFEFF){
+      if(readInt(arrayBuffer,0,2,1) === 65279){
         bufferView = bufferView.slice(1)
       }
     }else{
@@ -100,11 +111,12 @@ export const toString = function(arrayBuffer:ArrayBuffer|Uint16Array|Uint8Array,
     }
   }
   for (let i = 0; i < bufferView.length; i++) {
-    if(bufferView[i] === 0 && cleanZero){
+    if(bufferView[i] === 0 && cleanZero && i>=bufferView.length-2){
       continue
     }
     dataString += String.fromCharCode(bufferView[i]);
   }
+  // console.log(dataString)
   return dataString
 }
 
@@ -114,19 +126,23 @@ export const toString = function(arrayBuffer:ArrayBuffer|Uint16Array|Uint8Array,
  * js默认使用小端在前(utf16le)
  * @param str 
  * @param encode 
+ * @param addBOM default false 添加fffe或feff的字节顺序控制符(只用utf16le/utf16be编码有效) 当str不为空字符串才有效
+ * @param addEnd default false 添加0或00的结束控制字符
  */
-export const toArrayBuffer = function(str:string,encode:'ascii'|'utf16le'|'utf16be'):ArrayBuffer{
+export const toArrayBuffer = function(str:string,encode:'ascii'|'utf16le'|'utf16be',addBOM:boolean = false,addEnd:boolean = false):ArrayBuffer{
   let bufferView
   if(encode === 'ascii'){
-    bufferView = new Uint8Array(str.length)
+    bufferView = new Uint8Array(str.length + (addEnd?1:0))
     for(let i=0;i<str.length;i++){
       writeInt(bufferView,i,1,str.charCodeAt(i))
     }
   }else if(encode === 'utf16le'){
     // 小端在前
-    bufferView = new Uint8Array(str.length*2+2)
-    bufferView[0] = 0xFE
-    bufferView[1] = 0xFF
+    bufferView = new Uint8Array(str.length*2 + (addBOM?2:0) + (addEnd?2:0))
+    if(addBOM && str.length>0){
+      bufferView[0] = 0xFE
+      bufferView[1] = 0xFF
+    }
     // 因为writeInt写入是大端在前，之后统一反转
     for(let i=0;i<str.length;i++){
       writeInt(bufferView,i*2+2,2,str.charCodeAt(i))
@@ -134,9 +150,11 @@ export const toArrayBuffer = function(str:string,encode:'ascii'|'utf16le'|'utf16
     bufferView = utf16beToUTF16le(bufferView)
   }else if(encode === 'utf16be'){
     // 大端在前
-    bufferView = new Uint8Array(str.length*2+2)
-    bufferView[0] = 0xFE
-    bufferView[1] = 0xFF
+    bufferView = new Uint8Array(str.length*2 + (addBOM?2:0) + (addEnd?2:0))
+    if(addBOM && str.length>0){
+      bufferView[0] = 0xFE
+      bufferView[1] = 0xFF
+    }
     // 因为之后需要le转换
     for(let i=0;i<str.length;i++){
       writeInt(bufferView,i*2+2,2,str.charCodeAt(i))
@@ -149,14 +167,14 @@ export const toArrayBuffer = function(str:string,encode:'ascii'|'utf16le'|'utf16
 }
 
 /**
- * 从指定地方开始读取字符串，直到遇到0x00 或0x0000为止  
- * 返回bufferView 不会清除字节顺序标记  
- * ASCII时返回Uint8Array utf16时返回Uint16Array
+ * 从指定地方开始截取到0x00或0x0000为止  
+ * 不会清除字节顺序标记  
+ * 返回的arraybuffer不包括最后的0x00 或0x0000  
  * @param arrayBuffer 
  * @param start 
  * @param encode 
  */
-export const readStringToZero = function(arrayBuffer:ArrayBuffer,start:number,encode:'ascii'|'utf16'):Uint16Array|Uint8Array{
+export const sliceToZero = function(arrayBuffer:ArrayBuffer,start:number,encode:'ascii'|'utf16'):ArrayBuffer{
   let bufferView:Uint16Array|Uint8Array
   if(encode === 'ascii'){
     bufferView = <Uint8Array>getView(arrayBuffer,1)
@@ -167,10 +185,10 @@ export const readStringToZero = function(arrayBuffer:ArrayBuffer,start:number,en
   }
   for(let i=start;i<bufferView.length;i++){
     if(bufferView[i] === 0){
-      return bufferView.slice(start,i)
+      return bufferView.slice(start,i).buffer
     }
   }
-  return bufferView.slice(start)
+  return bufferView.slice(start).buffer
 }
 
 export const bufferToArrayBuffer = function(buffer:Buffer):ArrayBuffer {
@@ -180,4 +198,22 @@ export const bufferToArrayBuffer = function(buffer:Buffer):ArrayBuffer {
     bufferView[i] = buffer[i];
   }
   return arrayBuffer;
+}
+
+export const concat = function(...args:ArrayBuffer[]):ArrayBuffer{
+  let length = 0;
+  for (let arr of args) {
+    length += arr.byteLength;
+  }
+  const result = new ArrayBuffer(length)
+  const resultView = new Uint8Array(result)
+  let offset = 0
+  for(let item of args){
+    const view = new Uint8Array(item)
+    for(let i=0;i<view.length;i++){
+      resultView[i+offset] = view[i]
+    }
+    offset += view.length
+  }
+  return result
 }
